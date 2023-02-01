5462c6fb6a34c26e4ce2ef287bae464157f830bb
Rem3663 <rembrand@lepelaar.eu>
2023-02-01 10:53:41 +0100

    binary run data plus automated read file

--- Comparison/reader.py
+++ Comparison/reader.py
@@ -73,84 +73,141 @@ p5 = l5.profile_data()
 pDonor = lDonor.profile_data()
 pCompanion = lCompanion.profile_data()
 
+# determine what mass donor and companion star are initially
+Donor_star_mass_list = hDonor.star_mass
+Companion_star_mass_list = hCompanion.star_mass
+Donor_star_mass = Donor_star_mass_list[0]
+Companion_star_mass = Companion_star_mass_list[0]
+#print(Donor_star_mass, Companion_star_mass)
+
+# state what single star data to use (1,2,4,5 or 8 Msol)
+if Donor_star_mass <= 1:
+	hd = h1
+	ld = l1
+	pd = p1
+	Dmass = "1 Msol"
+if Donor_star_mass > 1 and Donor_star_mass <= 2:
+	hd = h2
+	ld = l2
+	pd = p2
+	Dmass = "2 Msol"
+if Donor_star_mass > 2 and Donor_star_mass <= 4:
+	hd = h4
+	ld = l4
+	pd = p4
+	Dmass = "4 Msol"
+if Donor_star_mass > 4 and Donor_star_mass <= 5:
+	hd = h5
+	ld = l5
+	pd = p5
+	Dmass = "5 Msol"
+if Donor_star_mass > 5:
+	hd = h8
+	ld = l8
+	pd = p8
+	Dmass = "8 Msol"
+
+# same for companion
+if Companion_star_mass <= 1:
+	hc = h1
+	lc = l1
+	pc = p1
+	Cmass = "1 Msol"
+if Companion_star_mass > 1 and Companion_star_mass <= 2:
+	hc = h2
+	lc = l2
+	pc = p2
+	Cmass = "2 Msol"
+if Companion_star_mass > 2 and Companion_star_mass <= 4:
+	hc = h4
+	lc = l4
+	pc = p4
+	Cmass = "4 Msol"
+if Companion_star_mass > 4 and Companion_star_mass <= 5:
+	hc = h5
+	lc = l5
+	pc = p5
+	Cmass = "5 Msol"
+if Companion_star_mass > 5:
+	hc = h8
+	lc = l8
+	pc = p8
+	Cmass = "8 Msol"
+	
+# set up figure
 plt.style.use("dark_background")
 figure, axis = plt.subplots(2, 2)
 
-#axis[0, 0].plot(h1.log_Teff, h1.log_L, label='1Msol', color='w', alpha=0.25)
-#axis[0, 0].plot(h2.log_Teff, h2.log_L, label='2Msol', color='w', alpha=0.25)
-#axis[0, 0].plot(h4.log_Teff, h4.log_L, label='4Msol', color='w', alpha=0.25)
-#axis[0, 0].plot(h8.log_Teff, h8.log_L, label='8Msol', color='w', alpha=0.25)
-axis[0, 0].plot(h5.log_Teff, h5.log_L, label='5Msol', color='w')
+# HRD plots
+axis[0, 0].plot(hd.log_Teff, hd.log_L, label='single star', color='w')
 axis[0, 0].plot(hDonor.log_Teff, hDonor.log_L, label='1', color='b')
-axis[0, 0].set_title("HR Diagram Donor Star")
+axis[0, 0].set_title(f"HR Diagram Donor Star ({Dmass})")
 axis[0, 0].set_xlabel("log Effective Temperature")
 axis[0, 0].set_ylabel("log Luminosity")
 axis[0, 0].invert_xaxis()
 
-#axis[0, 1].plot(h1.log_Teff, h1.log_L, label='1Msol', color='w', alpha=0.25)
-#axis[0, 1].plot(h2.log_Teff, h2.log_L, label='2Msol', color='w', alpha=0.25)
-#axis[0, 1].plot(h4.log_Teff, h4.log_L, label='4Msol', color='w', alpha=0.25)
-#axis[0, 1].plot(h8.log_Teff, h8.log_L, label='8Msol', color='w', alpha=0.25)
-axis[0, 1].plot(h1.log_Teff, h1.log_L, label='5Msol', color='w')
+axis[0, 1].plot(hc.log_Teff, hc.log_L, label='single star', color='w')
 axis[0, 1].plot(hCompanion.log_Teff, hCompanion.log_L, label='2', color='r')
-axis[0, 1].set_title("HR Diagram Companion Star")
+axis[0, 1].set_title(f"HR Diagram Companion Star ({Cmass})")
 axis[0, 1].set_xlabel("log Effective Temperature")
 axis[0, 1].set_ylabel("log Luminosity")
 axis[0, 1].invert_xaxis()
 
-#axis[1, 0].loglog(p1.Rho, p1.T, label='1Msol', color='w', alpha=0.25)
-#axis[1, 0].loglog(p2.Rho, p2.T, label='2Msol', color='w', alpha=0.25)
-#axis[1, 0].loglog(p4.Rho, p4.T, label='4Msol', color='w', alpha=0.25)
-#axis[1, 0].loglog(p8.Rho, p8.T, label='8Msol', color='w', alpha=0.25)
-axis[1, 0].loglog(p5.Rho, p5.T, label='5Msol', color='w')
+# TRho plots
+axis[1, 0].loglog(pd.Rho, pd.T, label='single star', color='w')
 axis[1, 0].loglog(pDonor.Rho, pDonor.T, label='1', color='b')
-axis[1, 0].set_title("TRho Graph Single Star")
+axis[1, 0].set_title(f"TRho Graph Donor Star ({Dmass})")
 axis[1, 0].set_xlabel("Density")
 axis[1, 0].set_ylabel("Temperature")
 
-#axis[1, 1].loglog(p1.Rho, p1.T, label='1Msol', color='w', alpha=0.25)
-#axis[1, 1].loglog(p2.Rho, p2.T, label='2Msol', color='w', alpha=0.25)
-#axis[1, 1].loglog(p4.Rho, p4.T, label='4Msol', color='w', alpha=0.25)
-#axis[1, 1].loglog(p8.Rho, p8.T, label='8Msol', color='w', alpha=0.25)
-axis[1, 1].loglog(p1.Rho, p1.T, label='5Msol', color='w')
+axis[1, 1].loglog(pc.Rho, pc.T, label='single star', color='w')
 axis[1, 1].loglog(pCompanion.Rho, pCompanion.T, label='2', color='r')
-axis[1, 1].set_title("TRho Graph Companion Star")
+axis[1, 1].set_title(f"TRho Graph Companion Star ({Cmass})")
 axis[1, 1].set_xlabel("Density")
 axis[1, 1].set_ylabel("Temperature")
-
-'''
-axis[2, 0].plot(hDonor.star_age, hDonor.star_mass, label='1', color='b')
-axis[2, 0].set_title("Mass over time Donor Star")
-axis[2, 0].set_xlabel("Time")
-axis[2, 0].set_ylabel("Mass")
-
-axis[2, 1].plot(hCompanion.star_age, hCompanion.star_2_mass, label='2', color='r')
-axis[2, 1].set_title("Mass over time Companion Star")
-axis[2, 1].set_xlabel("Time")
-axis[2, 1].set_ylabel("Mass")
-
-axis[3, 0].plot(hDonor.star_age, hDonor.log_R, label='1', color='b')
-axis[3, 0].set_title("Radius over time Donor Star")
-axis[3, 0].set_xlabel("Time")
-axis[3, 0].set_ylabel("Radius")
-
-axis[3, 1].plot(hCompanion.star_age, hCompanion.log_R, label='2', color='r')
-axis[3, 1].set_title("Radius over time Companion Star")
-axis[3, 1].set_xlabel("Time")
-axis[3, 1].set_ylabel("Radius")
-'''
 
 # Combine all the operations and display
 figure.tight_layout()
 plt.show()
 
-'''
-plt.plot(hDonor.log_Teff, hDonor.log_L, label='1', color='b')
-plt.plot(hCompanion.log_Teff, hCompanion.log_L, label='2', color='r')
-plt.title("HR Diagram Donor Star")
-plt.xlabel("log Effective Temperature")
-plt.ylabel("log Luminosity")
-plt.gca().invert_xaxis()
-plt.show()
-'''
 
+figure, axis = plt.subplots(3, 2)
+
+# Mass over time plots
+axis[0, 0].plot(hDonor.star_age, hDonor.star_mass, label='1', color='b')
+axis[0, 0].set_title(f"M/t Graph Donor Star ({Dmass})")
+axis[0, 0].set_ylabel("Mass")
+axis[0, 0].set_xlabel("Time")
+
+axis[0, 1].plot(hCompanion.star_age, hCompanion.star_mass, label='2', color='r')
+axis[0, 1].set_title(f"M/t Graph Companion Star ({Cmass})")
+axis[0, 1].set_ylabel("Mass")
+axis[0, 1].set_xlabel("Time")
+
+# distance and orbital period
+axis[1, 0].plot(hDonor.star_age, hDonor.period_days, label='1', color='b')
+axis[1, 0].set_title(f"Period/t Graph Donor Star ({Dmass})")
+axis[1, 0].set_ylabel("Period")
+axis[1, 0].set_xlabel("Time")
+
+axis[1, 1].plot(hCompanion.star_age, hCompanion.binary_separation, label='2', color='r')
+axis[1, 1].set_title(f"Seperation/t Graph Companion Star ({Cmass})")
+axis[1, 1].set_ylabel("Seperation")
+axis[1, 1].set_xlabel("Time")
+
+# star radii
+axis[2, 0].plot(hd.star_age, hd.log_R, label='single star', color='w')
+axis[2, 0].plot(hDonor.star_age, hDonor.log_R, label='1', color='b')
+axis[2, 0].set_title(f"M/t Graph Donor Star ({Dmass})")
+axis[2, 0].set_ylabel("Radius")
+axis[2, 0].set_xlabel("Time")
+
+axis[2, 1].plot(hc.star_age, hc.log_R, label='single star', color='w')
+axis[2, 1].plot(hCompanion.star_age, hCompanion.log_R, label='2', color='r')
+axis[2, 1].set_title(f"M/t Graph Companion Star ({Cmass})")
+axis[2, 1].set_ylabel("Radius")
+axis[2, 1].set_xlabel("Time")
+
+# Combine all the operations and display
+figure.tight_layout()
+plt.show()
